5d99d55f3d3ecd44730b5fa21c517b018bd12e8b
fdufault <fdufault@videotron.ca>
2012-12-23 16:31:19 -0500

    Set random player generated.

--- src/main/java/org/lardonsoft/lardonpokersimulator/main/App.java
+++ src/main/java/org/lardonsoft/lardonpokersimulator/main/App.java
@@ -1,27 +1,26 @@
 package org.lardonsoft.lardonpokersimulator.main;
 
+import java.util.Random;
 import org.lardonsoft.lardonpokersimulator.model.card.Card;
 import org.lardonsoft.lardonpokersimulator.player.Player;
-
 
 public class App {
 
     public static void main(String[] args) {
-        
-       Table t = new Table();
-        
-       t.generate(8);
-        
-       for(Player p : t.players)
-       {
-           System.out.println("Name: " + p.getName());
-           System.out.println("Token: " + p.getTokens());
-           
-           for(Card c : p.getCards())
-           {
-               System.out.println("\t" + c.toString());
-           }
-       }
-       
+        Random random = new Random();
+        int nbPlayer = random.nextInt(7) + 2;
+        Table t = new Table();
+        System.out.println("Random nb player: " + nbPlayer);
+        t.generate(nbPlayer);
+
+        for (Player p : t.players) {
+            System.out.println("Name: " + p.getName());
+            System.out.println("\tToken: " + p.getTokens());
+
+            for (Card c : p.getCards()) {
+                System.out.println("\t" + c.toString());
+            }
+        }
+
     }
 }
